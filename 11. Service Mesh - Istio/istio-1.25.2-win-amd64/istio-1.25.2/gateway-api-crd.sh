9363e1b3892b4d41b305208698794b7b2bc678ba
Faraz <farazhussain503@gmail.com>
2025-06-08 15:33:11 +0530

    first commit

---
+++ 11. Service Mesh - Istio/istio-1.25.2-win-amd64/istio-1.25.2/gateway-api-crd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+{ kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.2.1" | kubectl apply -f -; }
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# K8s-Hand-on - Need to convert docx documentations to readme
